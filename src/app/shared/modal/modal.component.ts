26a0a2f15bfe83cbc4cd5f5648fffd8a89980da1
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2022-04-16 20:53:21 +0200

    memory leak fixed and teleportation used for modal component

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -8,10 +8,12 @@ import { ModalService } from 'src/app/services/modal.service';
 })
 export class ModalComponent implements OnInit {
   @Input() modalID= ""
-  constructor(public modal : ModalService) {
-   }
+  constructor(public modal : ModalService, public el: ElementRef) {
+  
+  }
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 
   closeModal($event: Event) {
