70162a417b1cdc5bc31403e1d4e1b0121836596e
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2022-05-09 01:20:34 +0200

    added input masking

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -16,10 +16,25 @@ export class RegisterComponent {
     Validators.required,
     Validators.email,
   ])
-  age= new FormControl('')
-  password= new FormControl('')
-  confirm_password = new FormControl('')
-  phoneNumber= new FormControl('')
+  age= new FormControl('',[
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120),
+  ])
+  password= new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ])
+  confirm_password = new FormControl('',[
+    Validators.required,
+  ])
+  phoneNumber= new FormControl('',
+  [
+    Validators.required,
+    Validators.minLength(13),
+    Validators.maxLength(13),
+  ])
+  
 
 
   registerForm = new FormGroup({
