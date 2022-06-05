c8dcc7ce74236cc82843e8b2b77269b6ad9caab2
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2022-06-05 23:05:22 +0200

    added template forms for login section

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule , FormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { AlertComponent } from '../shared/alert/alert.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +16,8 @@ import { AlertComponent } from '../shared/alert/alert.component';
   imports: [
     CommonModule,
     SharedModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   exports: [
     AuthModalComponent
