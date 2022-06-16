d8c0ebf00cda35ce87faef5d4a37bba464ba57d5
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2022-06-17 01:27:51 +0200

    added Firebase for auth

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-register',
@@ -7,38 +9,34 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  constructor(
+   private auth: AuthService
+  ) {}
+    
 
-  name= new FormControl('',[
-    Validators.required,
-    Validators.minLength(3),
-  ])
-  email= new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ])
-  age= new FormControl('',[
+  inSubmission: boolean = false;
+
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  age = new FormControl('', [
     Validators.required,
     Validators.min(18),
     Validators.max(120),
-  ])
-  password= new FormControl('', [
+  ]);
+  password = new FormControl('', [
     Validators.required,
-    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
-  ])
-  confirm_password = new FormControl('',[
-    Validators.required,
-  ])
-  phoneNumber= new FormControl('',
-  [
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
+  ]);
+  confirm_password = new FormControl('', [Validators.required]);
+  phoneNumber = new FormControl('', [
     Validators.required,
     Validators.minLength(13),
     Validators.maxLength(13),
-  ])
-  
-  showAlert = false;
-  alertMsg = "Please wait ! Your account is being created.  " 
-  alertColor = "blue"
+  ]);
 
+  showAlert = false;
+  alertMsg = 'Please wait ! Your account is being created.  ';
+  alertColor = 'blue';
 
   registerForm = new FormGroup({
     name: this.name,
@@ -46,13 +44,31 @@ export class RegisterComponent {
     age: this.age,
     password: this.password,
     confirm_password: this.confirm_password,
-    phoneNumber: this.phoneNumber
+    phoneNumber: this.phoneNumber,
+  });
 
-  })
-  
-  register(){
+  async register() {
     this.showAlert = true;
-    this.alertMsg = "Please wait ! Your account is being created.  "
-    this.alertColor = "blue"
+    this.alertMsg = 'Please wait ! Your account is being created.  ';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+
+
+    try {
+        await this.auth.createUser(this.registerForm.value);
+
+
+
+    } catch (e) {
+      console.log(e);
+
+      this.alertMsg = 'Something went wrong. Please try again.';
+      this.alertColor = 'red';
+      this.inSubmission = false;
+      return;
+    }
+
+    this.alertMsg = 'Your account has been created.';
+    this.alertColor = 'green';
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCNfz5bMM6oyrJ8lqkzE2TC2re9tUnlTbQ",
+    authDomain: "clips-8e134.firebaseapp.com",
+    projectId: "clips-8e134",
+    storageBucket: "clips-8e134.appspot.com",
+    messagingSenderId: "102817821836",
+    appId: "1:102817821836:web:e1c7d869982c93df4ff7a9"
+  }
 };
