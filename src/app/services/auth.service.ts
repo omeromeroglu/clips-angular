25f345f8b06537dfbe63dc2b15f159c991050639
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2022-06-18 15:29:19 +0200

    added conditional rendered navbar links regarding auth status

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import IUser from '../models/users.model';
 
@@ -10,10 +12,15 @@ import IUser from '../models/users.model';
 })
 export class AuthService {
   private usersCollection : AngularFirestoreCollection<IUser>
+  public isAuthenticated$ : Observable<boolean>
+
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore) { 
       this.usersCollection = this.db.collection('users');
+      this.isAuthenticated$ = auth.user.pipe(
+        map(user => !!user)
+      )
     }
 
   public async createUser(userData: IUser) {
@@ -24,10 +31,19 @@ export class AuthService {
         userData.email,
         userData.password
       );
-    await this.usersCollection.add({
+
+     if(!userCred.user){
+        throw new Error("User can't be found");
+      }
+
+    await this.usersCollection.doc(userCred.user.uid).set({
       name: userData.name,
       email: userData.email,
       age: userData.age,
       phoneNumber: userData.phoneNumber,
-  }
-}
+  })
+
+  await userCred.user.updateProfile({
+    displayName: userData.name,
+  })
+}}
